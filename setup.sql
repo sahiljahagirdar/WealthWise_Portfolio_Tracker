60bf59dc90497a3f6fc4006a79d2c9951bc88ce4
Sahil <sahiljahagirdar91@gmail.com>
2025-11-22 16:56:58 +0530

    Add setup.sql (database schema and seed data)

---
+++ setup.sql
@@ -0,0 +1,35 @@
+CREATE TABLE users(
+id SERIAL PRIMARY KEY,
+name VARCHAR(100),
+email VARCHAR(100) UNIQUE,
+created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE transactions(
+id SERIAL PRIMARY KEY,
+user_id INT REFERENCES users(id) ON DELETE CASCADE,
+symbol VARCHAR (20),
+type VARCHAR(10) CHECK (TYPE IN ('BUY','SELL')),
+units NUMERIC (12,2),
+price NUMERIC (12,2),
+date DATE,
+created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE prices(
+symbol VARCHAR (20) PRIMARY KEY,
+price NUMERIC (12,2),
+updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE holdings(
+id SERIAL PRIMARY KEY,
+user_id INT REFERENCES users(id) ON DELETE CASCADE,
+symbol VARCHAR (20),
+units NUMERIC (12,2),
+avg_cost NUMERIC (12,2),
+updated_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO users(name, email)
+VALUES ('Admin', 'admin@admin.com');
